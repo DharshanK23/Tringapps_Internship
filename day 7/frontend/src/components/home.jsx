e2025ebaaa0203594fb5c95d36586d50108f7da2
DharshanRam Kumar K <dharshanramk@gmail.com>
2024-05-20 18:47:16 +0530

    update day 7

--- day 7/frontend/src/components/home.jsx
+++ day 7/frontend/src/components/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { DataGrid } from '@mui/x-data-grid';
-import { Button, IconButton } from '@mui/material';
+import { Button, Box,IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -36,13 +36,13 @@ function Home() {
   };
 
   const columns = [
-    { field:'name',headerName:'Name',width: 130 },
-    { field:'email',headerName:'Email',width: 200 },
-    { field:'gender',headerName:'Gender',width: 100 },
-    { field:'locat',headerName:'Location',width: 150 },
-    { field:'language',headerName:'Languages',width: 150 },
-    { field:'project',headerName:'Project',width: 150 },
-    { field: 'actions',headerName: 'Actions',width: 300,renderCell: (params) => (
+    { field:'name',headerName:'Name',width: 130,editable: true},
+    { field:'email',headerName:'Email',width: 200,editable: true },
+    { field:'gender',headerName:'Gender',width: 100,editable: true},
+    { field:'locat',headerName:'Location',width: 150,editable: true },
+    { field:'language',headerName:'Languages',width: 150,editable: true},
+    { field:'project',headerName:'Project',width: 150,editable: true},
+    { field: 'actions',headerName: 'Actions',width: 300,editable: true,renderCell: (params) => (
       <div>
           <IconButton onClick={() =>Update(params.row.id)} color="primary">
             <EditIcon />
@@ -59,7 +59,12 @@ function Home() {
       <Button onClick={() => navigate('/')} variant='contained' color="primary"style={{padding:"5px",marginTop:"10px",marginRight:"10px",marginBottom:"10px",marginLeft:"10px"}}>Registration Form</Button>
       <Button onClick={() => navigate('/add-user')} variant='contained' color="primary"style={{padding:"5px",marginTop:"10px",marginRight:"10px",marginBottom:"10px",marginLeft:"10px"}}>Delete Selected</Button>
       <div className='container'>
-        <DataGrid className='styl' rows={users} columns={columns} />
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid className='styl' rows={users} columns={columns} 
+        checkboxSelection
+        disableRowSelectionOnClick
+        />
+      </Box>
       </div>
     </div>
   );
